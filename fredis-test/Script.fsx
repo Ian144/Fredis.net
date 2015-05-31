48a81cec31d69aac9dc02c05fc7494458b5696af
Ian144 <ian144@hotmail.com>
2015-05-31 14:58:34 +0100

    moved all nunit tests to xunit

--- fredis-test/Script.fsx
+++
@@ -1,6 +0,0 @@
-﻿
-#load "fredis_test.fs"
-open fredis_test
-
-// Define your library scripting code here
-
--- fredis-test/Scripts/load-project.fsx
+++ fredis-test/Scripts/load-project.fsx
@@ -1,7 +1,6 @@
 // Warning: generated file; your changes could be lost when a new file is generated.
 #I __SOURCE_DIRECTORY__
 #load @"load-references.fsx"
-#load @"..\fredis_test.fs"
-      @"..\TestRespMsgProcessor.fs"
+#load @"..\TestRespMsgProcessor.fs"
       @"..\TestFredisCmds.fs"
       @"..\TestFredisCmdsFSCheck.fs"
